05da3e04d31880619bcffd79aa5c65d544d05575
prabhsingh14 <psingh10_be21@thapar.edu>
2025-02-11 17:03:16 +0530

    Login error resolved

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -87,7 +87,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
     const {email, username, password} = req.body
-    if(!email || !username){
+    if(!(email || username)){
         throw new ApiError(400, "Email or username is required")
     }
 
